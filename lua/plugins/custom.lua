74e67996bc39f012007e6f1c6d9e08f1b5fddc73
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2025-11-28 22:38:27 +0100

    remove duplicate telescope-file-browser

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -22,31 +22,6 @@ return {
     },
   },
 
-  {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-    config = function()
-      local fb_actions = require("telescope").extensions.file_browser.actions
-
-      require("telescope").setup {
-        extensions = {
-          file_browser = {
-            theme = "ivy",
-            hijack_netrw = true,
-            mappings = {
-              ["n"] = {
-                ["h"] = fb_actions.goto_parent_dir,
-                ["l"] = fb_actions.open_dir,
-                ["."] = fb_actions.toggle_hidden,
-              },
-            },
-          },
-        },
-      }
-      require("telescope").load_extension "file_browser"
-    end,
-  },
-
 {
   'nvim-telescope/telescope-live-grep-args.nvim',
   dependencies = {
