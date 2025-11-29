02e6ac88c55eb8839a99e29fb3ce6ae9f6c1db5c
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2025-11-29 15:01:21 +0100

    add triptych

---
+++ lua/plugins/triptych.lua
@@ -0,0 +1,26 @@
+return {
+  'ibdul/triptych.nvim', -- main repo = 'simonmclean/triptych.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- required
+    'nvim-tree/nvim-web-devicons', -- optional for icons
+    'antosha417/nvim-lsp-file-operations' -- optional LSP integration
+  },
+  opts = {  -- config options here
+    mappings  = {
+      quit = "<esc>",
+      copy = "y",
+    },
+    options = {
+      display_directory_in_title = true,
+      show_hidden = true,
+      collapse_dirs = false,
+      line_numbers = {
+        enabled = true,
+        relative = true,
+      },
+    },
+  },
+  keys = {
+    { '<leader>d', ':Triptych<CR>' },
+  },
+}
