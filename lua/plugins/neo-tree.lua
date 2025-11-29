7d9c943f3970a3ecfd1682112fc84b000546d174
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2025-11-29 14:57:50 +0100

    add custom neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,24 @@
+  return {
+    "nvim-neo-tree/neo-tree.nvim",
+    lazy = false,
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = "Neotree",
+    keys = {
+      { "<leader>dx", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+
+    },
+    opts = {
+      filesystem = {
+        window = {
+          mappings = {
+            ["<leader>dx"] = "close_window",
+          },
+        },
+      },
+    },
+  }
