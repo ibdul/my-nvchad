a5f356a091e052549d68dcf0c8d232fbfce6aac8
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2025-11-29 16:57:30 +0100

    add neo format

---
+++ lua/plugins/neoformat.lua
@@ -0,0 +1,10 @@
+return {
+  'sbdchd/neoformat',
+  event = "VeryLazy",
+  description = [[
+     A (Neo)vim plugin for formatting code.
+  ]],
+  -- config = function ()
+  --   require("neoformat").setup()
+  -- end,
+}
