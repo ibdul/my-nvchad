3d12d12934ad901aa433fc8117ffbd4789f9b602
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2025-12-01 00:39:20 +0100

    extract mini-completion key mapping util to be used everywhere

--- lua/key-mappings-custom.lua
+++ lua/key-mappings-custom.lua
@@ -145,5 +145,20 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 
 vim.keymap.set("n",'<leader>ca', vim.lsp.buf.code_action, {desc='[C]ode [A]ction'})
 
--- local cmp = require 'cmp'
--- vim.keymap.set("n",'<C-n>', cmp.mapping.select_next_item()) 
+-- completion
+local imap_expr = function(lhs, rhs)
+  vim.keymap.set('i', lhs, rhs, { expr = true })
+end
+imap_expr('<Tab>',   [[pumvisible() ? "\<C-n>" : "\<Tab>"]])
+imap_expr('<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]])
+
+_G.cr_action = function()
+  -- If there is selected item in popup, accept it with <C-y>
+  if vim.fn.complete_info()['selected'] ~= -1 then return '\25' end
+  -- Fall back to plain `<CR>`. You might want to customize according
+  -- to other plugins. For example if 'mini.pairs' is set up, replace
+  -- next line with `return MiniPairs.cr()`
+  return '\r'
+end
+
+vim.keymap.set('i', '<CR>', 'v:lua.cr_action()', { expr = true })
