1adf416db275f957b5afe70bec1c3258c0a11163
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2025-12-03 00:28:13 +0100

    cleanup plugins to fix issue with multiple signature popups

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,20 +1,5 @@
 return {
   {
-    "stevearc/conform.nvim",
-    tags = {
-      "lsp",
-      "linting",
-      "format",
-    },
-    description = [[
-      Lightweight yet powerful formatter plugin for Neovim
-    ]],
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
-
-  -- These are some examples, uncomment them if you want to see them work!
-  {
     "neovim/nvim-lspconfig",
     tags = {
       "lsp",
@@ -22,6 +7,8 @@ return {
     description = [[
       nvim-lspconfig. nvim-lspconfig is a collection of LSP server configurations for the Nvim LSP client. View all configs, or run :help lspconfig-all from Nvim.
     ]],
+    lazy = false,
+    -- enabled = false,
     config = function()
       require "configs.lspconfig"
     end,
