8e55ddae1e0a88077dde97d90cb9091bdff17436
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2025-12-04 23:44:29 +0100

    uncomment cmp in plugins

--- lua/plugins/tailwind-colorizer.lua
+++ lua/plugins/tailwind-colorizer.lua
@@ -19,8 +19,8 @@ return {
 			color_square_width = 2,
 		})
 
-		-- require("cmp").config.formatting = {
-		-- 	format = colorizer.formatter
-		-- }
+		require("cmp").config.formatting = {
+			format = colorizer.formatter
+		}
 	end
 }
