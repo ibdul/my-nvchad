934ec9d81d9d986f467a1710f16bb8f7b144e1ad
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2025-12-08 00:03:24 +0100

    install neoscroll

---
+++ lua/plugins/neoscroll.lua
@@ -0,0 +1,11 @@
+return {
+  "karb94/neoscroll.nvim",
+  tags = {
+    "ui",
+  },
+  description = [[
+    a smooth scrolling neovim plugin written in lua
+  ]],
+  event = "VeryLazy",
+  opts = {},
+}
