e065e7a067827a282685fe654d8610b1e2900f2d
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2025-12-08 00:17:15 +0100

    install neogen

---
+++ lua/plugins/neogen.lua
@@ -0,0 +1,13 @@
+return {
+  "danymat/neogen",
+  description = [[
+    Create annotations with one keybind, and jump your cursor in the inserted annotation
+    Defaults for multiple languages and annotation conventions
+    Extremely customizable and extensible
+    Written in lua (and uses Tree-sitter)
+  ]],
+  config = true,
+  event = "VeryLazy",
+  -- Uncomment next line if you want to follow only stable versions
+  version = "*",
+}
